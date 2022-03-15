16a4f3ce420c1c82c8b08b7a02ecc7313e22912a
Olga <ok2304@yandex.ru>
2022-03-15 22:09:51 +0300

    Allure + Jenkins

---
+++ src/test/java/mycompany/olga/tests/demoqa/TestBase.java
@@ -0,0 +1,41 @@
+package mycompany.olga.tests.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import mycompany.olga.helpers.Attach; //  путь!
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
+public class TestBase {
+    @BeforeAll
+    static void setUp() {
+        // для отображения шагов в аллюр отчете
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        // для запуска на удаленном селеноиде
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        // конфигурация удаленного запуска
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        // чтобы было видно, что происходит
+        capabilities.setCapability("enableVNC", true);
+        // для записи видео
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
